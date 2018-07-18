3712f5d533649a00fc074eb3f6c2dda0c0c3ae82
Patrick Schadler <patrick.schadler@gmail.com>
2018-07-18 22:16:46 +0200

    add firebase/vuefire configuration

--- src/pimlify-vue/src/main.ts
+++ src/pimlify-vue/src/main.ts
@@ -1,24 +1,27 @@
 import Vue from 'vue';
-import App from "./components/App.vue";
+import App from './components/App.vue';
 
 import Vuetify from 'vuetify';
+import VueFire from 'vuefire';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
-import 'vuetify/dist/vuetify.css'
+import 'vuetify/dist/vuetify.css';
 
-import './vuetify-fix.css'
+import './vuetify-fix.css';
 
-import store from './store'
+import store from './store';
+import { firebaseConfig } from './firebase-config';
 
 Vue.use(Vuetify);
+Vue.use(VueFire);
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
 
 new Vue({
   el: '#app',
   store,
   render: h => h(App)
 });
-
-// new Vue({
-//   el:'#app',
-//   components:{app},
-//   template: "<div><app></app></div>"
-// })
