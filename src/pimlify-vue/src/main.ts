9d9ea8dc8869fda74df51fcbdbea05f7861ba156
Opiskull <opi@peerzone.net>
2018-07-24 23:43:41 +0200

    feat(icons): add font awesome css and replace icon

--- src/pimlify-vue/src/main.ts
+++ src/pimlify-vue/src/main.ts
@@ -4,25 +4,24 @@ import Vuetify from 'vuetify';
 import App from './components/App.vue';
 
 import firebase from 'firebase';
+// tslint:disable-next-line:no-submodule-imports
 import 'firebase/firestore';
 import VueFire from 'vuefire';
+// tslint:disable-next-line:no-submodule-imports
 import 'vuetify/dist/vuetify.css';
 
 import './vuetify-fix.css';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBookOpen, faCoffee, faHome, faMinus, faPlus, faSave, faUtensils } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// tslint:disable-next-line:no-submodule-imports
+import '@fortawesome/fontawesome-free/css/all.css';
 
 import { firebaseConfig } from './firebase-config';
 import router from './router';
 import store from './store';
 
-library.add(faUtensils, faCoffee, faHome, faBookOpen, faPlus, faMinus, faSave);
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  iconfont: 'fa'
+});
 Vue.use(VueFire);
 Vue.use(VueRouter);
 
