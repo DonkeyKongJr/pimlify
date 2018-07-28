eccdbc03145b4e58b94df36ef4c316e57055bbfd
Patrick Schadler <patrick.schadler@gmail.com>
2018-07-28 16:30:37 +0200

    add jest packages and unit test example

---
+++ src/pimlify-vue/test/reset-password.test.ts
@@ -0,0 +1,13 @@
+import 'jest';
+import Vue from 'vue';
+import ResetPassword from '../src/components/reset-password';
+
+describe('ResetPassword', () => {
+  it('1 plus 1 should be 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it('clear should call clear on form', () => {
+    // add test logic here
+  });
+});
