f44dc448291376784f2b4d2930d959e91828b0c6
Patrick Schadler <patrick.schadler@gmail.com>
2018-07-31 20:14:01 +0200

    feat(unit-test): add test for sendPasswordResetEmail

--- src/pimlify-vue/src/components/__tests__/reset-password.test.ts
+++ src/pimlify-vue/src/components/__tests__/reset-password.test.ts
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import ResetPasswordComponent from '../reset-password-component';
 import ResetPassword from '../ResetPassword.vue';
+import * as firebase from 'firebase';
 
 Vue.config.silent = true;
 
@@ -11,10 +12,21 @@ Vue.use(Vuetify);
 describe('ResetPassword', () => {
   let component: Wrapper<ResetPasswordComponent>;
   let shallowComponent: Wrapper<ResetPasswordComponent>;
+  const sendPasswordResetEmail = jest.fn(() => Promise.resolve());
 
   beforeEach(() => {
     component = mount<ResetPasswordComponent>(ResetPassword);
     shallowComponent = shallowMount<ResetPasswordComponent>(ResetPassword);
+
+    const initializeApp = jest.spyOn(firebase, 'initializeApp').mockImplementation(() => {
+      return {
+        auth: () => {
+          return {
+            sendPasswordResetEmail
+          };
+        }
+      };
+    });
   });
 
   it('email should be invalid', () => {
@@ -27,14 +39,22 @@ describe('ResetPassword', () => {
     expect(component.vm.valid).toBeTruthy();
   });
 
-  it('reset should not call sendPasswordResetEmail when form is not valid', () => {
+  it('reset should call sendPasswordResetEmail when form is valid', () => {
     const formMock = {
       validate: () => {
-        return false;
+        return true;
       }
     };
 
+    jest.spyOn(firebase, 'auth').mockImplementation(() => {
+      return {
+        sendPasswordResetEmail
+      };
+    });
+
     shallowComponent.vm.$refs.form = formMock as any;
     shallowComponent.vm.reset();
+
+    expect(firebase.auth().sendPasswordResetEmail).toHaveBeenCalled();
   });
 });
